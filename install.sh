2f223941d024d407af352864dcdc45fdda22b30e
DarthChungo <antonio@adaro.eu>
2021-11-15 19:37:27 +0100

    Consolidated code into one file

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-chmod +x unified-kernel-gen
-chmod +x unified-kernel-remove
+chmod +x unified-kernel-manager
 
-cp unified-kernel-gen /sbin
-cp unified-kernel-remove /sbin
+cp unified-kernel-manager /sbin
 cp 90-unified-kernel-gen.hook /etc/pacman.d/hooks/
 cp 90-unified-kernel-remove.hook /etc/pacman.d/hooks/
 
