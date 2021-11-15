26c2ea66ed26efae3a1d7e31a128a92a8d345049
DarthChungo <antonio@adaro.eu>
2021-11-16 00:07:53 +0100

    Sign functionality

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ chmod +x unified-kernel-manager
 cp unified-kernel-manager /sbin
 cp 90-unified-kernel-gen.hook /etc/pacman.d/hooks/
 cp 90-unified-kernel-remove.hook /etc/pacman.d/hooks/
+cp 95-unified-kernel-sign.hook /etc/pacman.d/hooks
 
 echo "Installed files"
